5528d95aa507cae8276a9677dcc0736b8540054e
atishayjain2003 <atishayjain2003@gmail.com>
2024-11-07 02:55:16 +0530

    Admin registration

--- patient_management/patients/admin.py
+++ patient_management/patients/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
-from .models import Patient, PatientHistory
+from .models import (
+    Patient, 
+    PatientHistory, 
+    NewRecommendationsStage, 
+    FollowUpStage, 
+    ClinicalInterventionStage, 
+    QuotationPhaseStage, 
+    ReadyToScheduleStage, 
+    PreAdmissionPrepStage, 
+    PostponedAdmissionsStage, 
+    ClinicalStage, 
+    InitialTransitionStage, 
+    FinalTransitionStage, 
+    ClosedStage
+)
+
+# Registering all models in the admin interface
 admin.site.register(Patient)
 admin.site.register(PatientHistory)
-
-
-# Register your models here.
+admin.site.register(NewRecommendationsStage)
+admin.site.register(FollowUpStage)
+admin.site.register(ClinicalInterventionStage)
+admin.site.register(QuotationPhaseStage)
+admin.site.register(ReadyToScheduleStage)
+admin.site.register(PreAdmissionPrepStage)
+admin.site.register(PostponedAdmissionsStage)
+admin.site.register(ClinicalStage)
+admin.site.register(InitialTransitionStage)
+admin.site.register(FinalTransitionStage)
+admin.site.register(ClosedStage)
